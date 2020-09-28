c5fb9e214d9a2d58df1a695fe598133b35f30888
Luiz Alberto Mesquita <luizalbertobm@gmail.com>
2020-09-28 22:23:58 +0100

    replace font-awsome for svg icons

--- index.php
+++ index.php
@@ -140,16 +140,7 @@ $mysqli = new mysqli("localhost", "root", "root");
 
   <!-- Favicons -->
   <link rel="apple-touch-icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-  <link rel="icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-  <link rel="icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-  <link rel="manifest" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/manifest.json">
-  <link rel="mask-icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-  <link rel="icon" href="https://getbootstrap.com/docs/4.5/assets/img/favicons/favicon.ico">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.57.0/codemirror.min.css" integrity="sha512-/BlxZbYLtYGExexketXsTi47eHp+r2kTeq2OHecQPZlfbc7WFXVrwbVW9HOYjI6c9Ti+P60ASmVLxittZ0EBGw==" crossorigin="anonymous" />
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.57.0/theme/darcula.min.css" integrity="sha512-kqCOYFDdyQF4JM8RddA6rMBi9oaLdR0aEACdB95Xl1EgaBhaXMIe8T4uxmPitfq4qRmHqo+nBU2d1l+M4zUx1g==" crossorigin="anonymous" /> -->
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.57.0/theme/ambiance.min.css" integrity="sha512-wVrMpCao81zIdzAM+fYyuF6BoUXjDO8le2mQXl4iVezdDSgZOCFtpm/AL3LZn9dqc3e4CloZfiZxhTp5UNpWIw==" crossorigin="anonymous" /> -->
-
-  <meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
   <meta name="theme-color" content="#563d7c">
 
 
@@ -180,13 +171,26 @@ $mysqli = new mysqli("localhost", "root", "root");
       box-shadow: 3px 3px 5px rgba(0, 0, 0, .1);
     }
 
-    .card a:hover i {
-      color: #ff9900 !important;
-    }
-
     .CodeMirror {
       border: 1px solid #666;
       height: 500px;
+    }
+    
+    .projects a, .projects a svg{
+      color: #666666;
+    }
+
+    .projects a:hover{
+      text-decoration: none;
+    }
+
+    .projects a .card{
+      transition: all .1s ease-in;
+    }
+
+    .projects a:hover .card{
+      /* background-color: #eeeeee; */
+      transform: scale(1.05);
     }
 
     @media (min-width: 768px) {
@@ -223,19 +227,19 @@ $mysqli = new mysqli("localhost", "root", "root");
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="head">
       <div class="container">
-        <h1 class="display-3">PHP Dashboard</h1>
+        <h1 class="display-4">PHP Dashboard</h1>
         <p>A dashboard to be used as a root page for php/localhost servers, giving some shortcuts and useful tools to developers.</p>
         <p>
-          <a id="btn-phpinfo" class="btn btn-primary btn-lg" href="javascript:void(0)" data-toggle="modal" data-target="#phpInfo">
+          <a id="btn-phpinfo" class="btn btn-primary mb-1" href="javascript:void(0)" data-toggle="modal" data-target="#phpInfo">
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
               <path fill="currentColor" d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
-            </svg> PHP Info</a>
-          <a id="btn-phpini" class="btn btn-info btn-lg" href="javascript:void(0)" data-toggle="modal" data-target="#phpIni">
+            </svg> Info</a>
+          <a id="btn-phpini" class="btn btn-info mb-1" href="javascript:void(0)" data-toggle="modal" data-target="#phpIni">
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
               <path fill="currentColor" d="M14 2H6C4.89 2 4 2.9 4 4V20C4 21.11 4.89 22 6 22H18C19.11 22 20 21.11 20 20V8L14 2M18 20H6V4H13V9H18V20M9.54 15.65L11.63 17.74L10.35 19L7 15.65L10.35 12.3L11.63 13.56L9.54 15.65M17 15.65L13.65 19L12.38 17.74L14.47 15.65L12.38 13.56L13.65 12.3L17 15.65Z" />
             </svg>
             PHP.ini</a>
-          <a id="btn-adminer" class="btn btn-success btn-lg" href="php-dashboard/adminer" role="button">
+          <a id="btn-adminer" class="btn btn-success mb-1" href="php-dashboard/adminer" role="button">
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
               <path fill="currentColor" d="M4,14V17C4,19 7.05,20.72 11,21V18.11L11.13,18C7.12,17.76 4,16.06 4,14M12,13C7.58,13 4,11.21 4,9V12C4,14.21 7.58,16 12,16C12.39,16 12.77,16 13.16,16L17,12.12C15.4,12.72 13.71,13 12,13M12,3C7.58,3 4,4.79 4,7C4,9.21 7.58,11 12,11C16.42,11 20,9.21 20,7C20,4.79 16.42,3 12,3M21,11.13C20.85,11.13 20.71,11.19 20.61,11.3L19.61,12.3L21.66,14.3L22.66,13.3C22.87,13.1 22.88,12.76 22.66,12.53L21.42,11.3C21.32,11.19 21.18,11.13 21.04,11.13M19.04,12.88L13,18.94V21H15.06L21.12,14.93L19.04,12.88Z" />
             </svg>
@@ -265,34 +269,36 @@ $mysqli = new mysqli("localhost", "root", "root");
             case 'txt':
             case 'php':
             case 'js':
-              $icon = 'fa-file-code-o';
+            case 'html':
+              $icon = '<svg style="width:48px;height:48px" viewBox="0 0 24 24"><path fill="#0088ff" d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M6.12,15.5L9.86,19.24L11.28,17.83L8.95,15.5L11.28,13.17L9.86,11.76L6.12,15.5M17.28,15.5L13.54,11.76L12.12,13.17L14.45,15.5L12.12,17.83L13.54,19.24L17.28,15.5Z" /></svg>';
               break;
             case 'dir':
-              $icon = 'fa-folder';
+              $icon = '<svg style="width:48px;height:48px" viewBox="0 0 24 24"><path fill="currentColor" d="M15.25,13H17.25L16.75,15H14.75L15.25,13M22,8V18A2,2 0 0,1 20,20H4A2,2 0 0,1 2,18V6C2,4.89 2.89,4 4,4H10L12,6H20A2,2 0 0,1 22,8M20,12H18.5L19,10H18L17.5,12H15.5L16,10H15L14.5,12H13V13H14.25L13.75,15H12V16H13.5L13,18H14L14.5,16H16.5L16,18H17L17.5,16H19V15H17.75L18.25,13H20V12Z" /></svg>';
               break;
             case 'jpg':
             case 'png':
             case 'gif':
             case 'svg':
             case 'bmp':
-              $icon = 'fa-file-image-o';
+              $icon = '<svg style="width:48px;height:48px" viewBox="0 0 24 24"><path fill="currentColor" d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M6,20H15L18,20V12L14,16L12,14L6,20M8,9A2,2 0 0,0 6,11A2,2 0 0,0 8,13A2,2 0 0,0 10,11A2,2 0 0,0 8,9Z" /></svg>';
               break;
             default:
-              $icon = 'fa-file';
+              $icon = '<svg style="width:48px;height:48px" viewBox="0 0 24 24"><path fill="currentColor" d="M13,9V3.5L18.5,9M6,2C4.89,2 4,2.89 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2H6Z" /></svg>';
               break;
           }
           ?>
 
-          <div class="col-md-4">
-            <div class="card  mb-4">
+          <div class="col-md-4 projects">
+          <a href="<?= $file ?>">
+          <div class="card  mb-4">
               <div class="row no-gutters">
                 <div class="col-auto pt-3 pb-3 pl-3">
-                  <a href="<?= $file ?>"><i class="fa fa-3x <?= $icon  ?> text-secondary" aria-hidden="true"></i></a>
+                  <?= $icon ?>
                 </div>
                 <div class="col-auto">
                   <div class="card-body p-3">
                     <p class="card-text">
-                      <strong><?= $file ?></strong> <br>
+                      <strong><?= $file ?></strong><br>
                       <small class="text-muted">Size: <?= $file_ext == 'dir' ? get_directory_size($file) : display_size(filesize($file)) ?></small>
                     </p>
                   </div>
@@ -304,11 +310,6 @@ $mysqli = new mysqli("localhost", "root", "root");
         <?php } ?>
       </div>
 
-      <h3>Apache Loaded Modules:</h3>
-      <?php 
-      $apacheModules = apache_get_modules();
-      echo implode(', ', $apacheModules);
-      ?>
       <hr>
 
     </div> <!-- /container -->
@@ -385,15 +386,14 @@ $mysqli = new mysqli("localhost", "root", "root");
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
-  <script src="https://use.fontawesome.com/c41c56d25a.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.57.0/codemirror.min.js" integrity="sha512-K8GMktcEqOI28I3f5C6kraFm1F4wVLvnBxGU+imS/zOLT1COAT799Ln4DJyAbpdyNt6LgMIWcwy4ptCYXCIDqA==" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.57.0/mode/properties/properties.min.js" integrity="sha512-KFE2cwTwAMmZ2uQzOh8sRi0pBCHJBXyzbhhELf7gfx0n/m8no2tQpEQDW8dxSNCO1jaNAmFJ5KuXfZ/oCjoLDQ==" crossorigin="anonymous"></script>
 
   <script>
     //window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')
     $('#filter').keyup(function() {
-      $('.card:not(:contains(' + $(this).val() + '))').parent().hide();
-      $('.card:contains(' + $(this).val() + ')').parent().show();
+      $('.card:not(:contains(' + $(this).val() + '))').parent().parent().hide();
+      $('.card:contains(' + $(this).val() + ')').parent().parent().show();
     })
 
 
